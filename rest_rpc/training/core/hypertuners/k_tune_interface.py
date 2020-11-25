5e82becead8a7a4cbe1cfdc98c873b1417efc78b
Kelvin Soh <kelvin_soh@aiap.sg>
2020-11-25 10:38:21 +0800

    testing evaluate consumer with custom callback

--- rest_rpc/training/core/hypertuners/k_tune_interface.py
+++ rest_rpc/training/core/hypertuners/k_tune_interface.py
@@ -9,12 +9,12 @@
 # Libs
 
 # Custom
-from rest_rpc.training.core.hypertuners.k_tune_driver_script import start_generate_hp, start_training_hp
+from rest_rpc.training.core.hypertuners.k_tune_driver_script import start_generate_hp, start_training_hp, start_hp_validations
 from rest_rpc import app
 from rest_rpc.connection.core.utils import (
     RunRecords
 )
-
+from manager.completed_operations import CompletedConsumerOperator
 
 ##################
 # Configurations #
@@ -23,11 +23,11 @@ from rest_rpc.connection.core.utils import (
 db_path = app.config['DB_PATH']
 run_records = RunRecords(db_path=db_path)
 
-n_samples=3
+n_samples=2
 
 search_space = {
     'algorithm': 'FedProx',
-    'rounds': {"type": "choice", "values": [1,2]},
+    'rounds': {"type": "choice", "values": [1,1]},
     'epochs': 1,
     'lr': 0.001,
     'weight_decay': 0.0,
@@ -54,6 +54,18 @@ kwargs = {
     "search_space": search_space
 }
 
+def test(loaded_kwargs, host):
+    print("loaded_kwargs: ", loaded_kwargs)
+    # loaded_kwargs =  "TRAINING COMPLETE -  test_project_1/test_experiment_1/optim_run_5c68e185-c28f-4159-8df4-2504ce94f4c7"
+    status, project_expt_run = loaded_kwargs.split(' - ')
+    project_id, expt_id, run_id = project_expt_run.strip().split('/')
+    if status.split()[0] == 'TRAINING':
+        print("STARTING hp validations")
+        print(project_id, expt_id, run_id)
+        start_hp_validations(project_id, expt_id, run_id, None)
+    else:
+        print("NOT TRAINING. pass..")
+
 if __name__ == '__main__':
     # Generate hyperparameters
     start_generate_hp(kwargs)
@@ -70,4 +82,6 @@ if __name__ == '__main__':
     # and run evaluation based on the project_id, expt_id and run_id
     # ..
 
-
+    print("Complete consumer listening..")
+    completed_consume = CompletedConsumerOperator(host=app.config["SYN_MQ_HOST"])
+    completed_consume.listen_message(test)
